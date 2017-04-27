66a654847d023c4a95944d99ceec0679d715c6ee
BVal <mp16bvf@antares.dci.uia.mx>
2017-04-27 16:52:22 -0500

    Descripcion y bibliotecas.

--- Final.c
+++ Final.c
@@ -1,2 +1,22 @@
-//Final.c
+/*
+*    @File/archivo       Final.c 
+*    @Brief/resumen      El objetivo del programa es formar 3 módulos que permitan la
+*                        creación, aplicación y valoración de un examen.
+*                        El máximo de preguntas admitidas en la base de datos será de 
+*                        50. 
+*                        
+*    @Author/autor        Bruno Valerio Fernández, Abdín Alejandro Alcazar Terán, Benjamín Menchaca Reyna
+*    @Date/fecha          14-05-17
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void GENERADOR(void);
+void APLICADOR(void);
+
+int main(void)
+{
+	return 0;
+}
 
