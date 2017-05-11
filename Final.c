d23de9129847f582d853a24ff53849c21bf17dc8
BVal <mp16bvf@antares.dci.uia.mx>
2017-05-11 13:17:51 -0500

    pendiente incluir preguntas aleatorias en el generador de examenes.

--- Final.c
+++ Final.c
@@ -15,14 +15,11 @@
 
 char CSORT(int num);
 void RVALUE(int numlin, int rv[]);
-void RVALANS(int numlin, int rv[]);
 
 void creadorExamenes(void);
 void aplicadorExamenes(void);
 void calificadorExamenes(void);
 int archivoLineas(char nombreArchivo[30]);
-int lineasAleatorias(int numeroAleatorio,int contador,char nombreExamen_1[30]);
-int lineasAleatorias2(char nombreExamen_1[30]);
 
 struct ainfo{
 	char l;
@@ -42,9 +39,8 @@ typedef struct bracket{
 int main(void)
 {
 	char opcion;	
-
-	system("clear");
 	do{
+		system("clear");
 		printf("\n\tBienvenido.\n");
 		printf("\tQue deseas, aplicar o hacer un examen?\n");
 		printf("\tPresiona una opcion para continuar.\n");
@@ -58,8 +54,7 @@ int main(void)
 		{
 			case '1': creadorExamenes();
 				break;
-			case '2':aplicadorExamenes();
-        			calificadorExamenes();		
+			case '2':aplicadorExamenes();	
 				break;
 			case 's':break;
 			default: system("clear");
@@ -70,20 +65,18 @@ int main(void)
 
 
 /*
-*	@Brief/resumen		La función permite generar un examen basado en las 
-*				características que le dé el usuario.
-*				Es importante mencionar que cada pregunta e inciso llevará un 
-*				formato especial para su lectura en el siguiente 
-*				segmento.
+*	@Brief/resumen		Esta funcion genera un examen basado en las caracteristicas que le da un usuario. Se pide un nombre de salida, un archivo de origen o banco y
+				el numero de preguntas que contendra el examen.
+*				
 *	@Author/autor		Bruno Valerio Fernández, Abdín Alejandro Alcazar Terán, Benjamín Menchaca Reyna	
 *	@Return 		void
 */
-void creadorExamenes(void){
-
+void creadorExamenes(void)
+{
 	FILE *fp_in;
 	FILE *fp_out;
 
-	int cont;
+	int cont, cont2=0;
 	int numeroPreguntas,numeroLineas;
 	char filename[30];
 	char nuevoExamen[30];
@@ -102,6 +95,11 @@ void creadorExamenes(void){
 	}
 
 	numeroLineas=archivoLineas(filename);
+	if(numeroLineas<5)
+	{
+		printf("\n\tEs recomendable tener un minimo de 5 preguntas escritas en el banco de preguntas.\n\tAgregar mas preguntas o utilizar otro archivo para continuar.\n");
+		exit(1);
+	}
 	do{
 		system("clear");
 		printf("\n[Creador de Examenes]\n\n");
@@ -121,8 +119,23 @@ void creadorExamenes(void){
 	getchar();
 	strcat(nuevoExamen,"\0");
 	fp_out=fopen(nuevoExamen,"wt");
+/*
+	int rvalue[numeroLineas];
+	RVALUE(numeroLineas, rvalue);
 
-	for(cont=0;cont<numeroPreguntas;cont++)
+	for(cont=0;cont<numeroPreguntas;cont++)//Es
+	{
+		fgets(lineaArchivo,300,fp_in);
+		cont2++;
+		if(rvalue[cont]==cont)
+		{
+			fprintf(fp_out,"%d,",cont+1);
+			fputs(lineaArchivo,fp_out);
+			cont2=0;
+		}
+	}
+*/
+	for(cont=0;cont<numeroPreguntas;cont++)//Es
 	{
 		fgets(lineaArchivo,300,fp_in);
 		fprintf(fp_out,"%d,",cont+1);
@@ -136,39 +149,32 @@ void creadorExamenes(void){
 }
 
 /*
-*	@Brief/resumen		La función permite hacer una recopilación de todas las 			
-*				preguntas y las muestra en pantalla.
-*				Al principio, se pedirán los datos completos del alumno, nombre 
-*				y número de cuenta.
-*				Los datos del alumno al concluir el test se verán guardados en 
-*				un archivo, en él estarán guardadas las respuestas del alumno. 
-*				
+*	@Brief/resumen		Esta funcion recibe las preguntas del archivo indicado y las
+*				intercala en orden aleatorio, al igual que sus respectivas respuestas.
+*				Los datos del alumno al concluir el test son guardados en 
+*				un archivo.			
 *	@Author/autor		Bruno Valerio Fernández, Abdín Alejandro Alcazar Terán, Benjamín Menchaca Reyna
-*	
 *	@Return 		void
 */
 void aplicadorExamenes(void)
 {
 	int rvalans[4];	
 	int cont, cont2, score=0, totscore=0, key=0;;
-	char r;
+	char r, numeroLineas;
 	
 	FILE *fpin, *fp_out, *sbank;
 
 	int idPregunta;
 	char nombreExamen_1[30];
 	char numeroCuenta[10];
-	char lineaAlumnos[100];
+	char lineaAlumnos[100], buff[150];
 	char cuentaAlumno[10],nombreAlumno[30];
-//IDENTIFICADOR DEL EXAMEN|CUENTA|CALIFICACIÓN|TIEMPO|RESPUESTAS|CORRECTAS(#)|PREGUNTAS(#)|TIEMPO TOTAL| MÁXIMO DE PUNTOS QUE SE PUEDE TENER
-
-	int valorNeto=0;
-	int numeroLineas;
 
 	double segundos = 0, milis=0;
-	struct timeval final, inicio;
 
+	struct timeval final, inicio;
  	srand(time(0));
+
 	printf("\n\t[Aplicador de Examenes]\n");
 	if((sbank=fopen("bancoAlumnos.csv","rt"))==NULL)
 	{
@@ -180,25 +186,29 @@ void aplicadorExamenes(void)
 	printf("\tIngrese su número de cuenta: ");
 	scanf("%s",numeroCuenta);
 	getchar();
+
 	while((fgets(lineaAlumnos,100,sbank))!=NULL)
 //	while(fscanf(sbank,"%[^,],%[^,],\n",cuentaAlumno,nombreAlumno)>=0)//Forma alternativa de lectura de campos del archivo. Si recibe los campos pero no entra en la condicion 'if((strcmp...)
 	{
-/**/		sscanf(lineaAlumnos,"%[^,],%[^,],\n",cuentaAlumno,nombreAlumno);
-//puts(cuentaAlumno);
-//puts(nombreAlumno);
+
+		sscanf(lineaAlumnos,"%[^,],%[^,],",cuentaAlumno,nombreAlumno);
 		if((strcmp(numeroCuenta,cuentaAlumno))==0)
 		{
+
+			for(cont=0;cont<30;cont++) 
+				if(nombreAlumno[cont]=='\n') 
+					nombreAlumno[cont]='\0';	
 			printf("\tUsuario: %s\n",nombreAlumno);
 			key=1;
 			break;
 		}
 	}
+
 	if(key==0)
 	{
 		printf("\tEl usuario ingresado no existe.\n");
 		exit(1);
 	}
-
 	
 	printf("\tIngrese el nombre del examen: ");
 	scanf("%s", nombreExamen_1);
@@ -216,8 +226,6 @@ void aplicadorExamenes(void)
 	for(cont=0;cont<numeroLineas;cont++)
 	{
 		fscanf(fpin,"%d,%[^,],%[^,],%[^,],%[^,],%[^,],%d", &qlist[cont].qnum, qlist[cont].q, qlist[cont].cans.ans, qlist[cont].b.ans, qlist[cont].c.ans, qlist[cont].d.ans, &qlist[cont].pts);
-
-//printf("%d, %s, %s, %s, %s, %s, %d\n",qlist[cont].qnum, qlist[cont].q, qlist[cont].cans.ans,qlist[cont].b.ans,qlist[cont].c.ans,qlist[cont].d.ans,qlist[cont].pts);
 	}
 	//rvalue= malloc(numeroLineas*sizeof(int));
 	int rvalue[numeroLineas];
@@ -226,29 +234,28 @@ void aplicadorExamenes(void)
 	system("clear");
 	printf("\tEXAMEN:\n");
 
-	gettimeofday(&inicio, NULL);
+	gettimeofday(&inicio, NULL);//Se comienza a contar el tiempo del examen.
 
 	for(cont=0;cont<numeroLineas;cont++)
 	{
-//printf("%d\n",cont);
-		RVALANS(4, rvalans);
+		RVALUE(4, rvalans);
 		printf("\nPregunta %d:\t\tValor:%d\n",cont+1,qlist[rvalue[cont]].pts);
 		printf("%s\n",qlist[rvalue[cont]].q);
 		for(cont2=0;cont2<4;cont2++)
 		{
-			if(rvalans[cont2]==1){
+			if(rvalans[cont2]==0){
 				qlist[rvalue[cont]].cans.l=CSORT(cont2);
 				printf("%c) %s\n", qlist[rvalue[cont]].cans.l,qlist[rvalue[cont]].cans.ans);	
 			}
-			if(rvalans[cont2]==2){
+			if(rvalans[cont2]==1){
 				qlist[rvalue[cont]].b.l=CSORT(cont2);
 				printf("%c) %s\n", qlist[rvalue[cont]].b.l,qlist[rvalue[cont]].b.ans);	
 			}
-			if(rvalans[cont2]==3){
+			if(rvalans[cont2]==2){
 				qlist[rvalue[cont]].c.l=CSORT(cont2);
 				printf("%c) %s\n", qlist[rvalue[cont]].c.l,qlist[rvalue[cont]].c.ans);	
 			}
-			if(rvalans[cont2]==4){
+			if(rvalans[cont2]==3){
 				qlist[rvalue[cont]].d.l=CSORT(cont2);
 				printf("%c) %s\n", qlist[rvalue[cont]].d.l,qlist[rvalue[cont]].d.ans);	
 			}
@@ -262,30 +269,41 @@ void aplicadorExamenes(void)
 		totscore+= qlist[rvalue[cont]].pts;
 	}
 	
-	gettimeofday(&final, NULL);
+	gettimeofday(&final, NULL);//Se termina el tiempo del examen.
 	segundos = (double)(final.tv_usec - inicio.tv_usec) / 1000000 + (double)(final.tv_sec - inicio.tv_sec);
-	printf("\n\nTotal: %d\ttotalpuntos: %d\ntiempo: %f segundos\n", score, totscore,segundos);
-
-
-	fp_out=fopen("informeAlumno.txt","a+");
-	while(fgets(lineaAlumnos,100,fpin)!=NULL)
+	
+	if((fp_out=fopen("informeAlumno.txt","rt"))!=NULL)
 	{
-		sscanf(lineaAlumnos,"%[^,],%[^\n],\n",cuentaAlumno,nombreAlumno);		
-		if((strcmp(cuentaAlumno,numeroCuenta))==0)
-		{	
-			printf("El examen no es valido, ya hay un resultado previo.\n");
-			exit(1);
+		while(fgets(lineaAlumnos,100,fp_out)!=NULL)
+		{
+			sscanf(lineaAlumnos,"%[^,],%[^\n],\n",cuentaAlumno, buff);
+			if((strcmp(cuentaAlumno,numeroCuenta))==0)
+			{	
+				printf("El examen no es valido, ya hay un resultado previo.\n");
+				exit(1);
+			}
 		}
+		fclose(fp_out);
 	}
+	fp_out=fopen("informeAlumno.txt","a");
 	
-	fprintf(fp_out,"Matricula: %s,Nombre: %s,Examen: %s,Calificacion: %d, PuntosTotales: %d, PuntoObtenidos: %d, Tiempo: %f",cuentaAlumno,nombreAlumno, nombreExamen_1, (score/totscore*10),totscore, score, segundos); 
+	fprintf(fp_out,"%s,%s,%s,%d,%d,%d,%f,\n",cuentaAlumno,nombreAlumno, nombreExamen_1, (score/totscore*10),totscore, score, segundos); 
 	printf("\n\n");
-	
 	fclose(fpin);
 	fclose(fp_out);
+
+	printf("\t[Calificador]\n\n");
+	printf("Calificacion: %d\n\nPuntos Obtenidos: %d\nPuntos Totales: %d\nTiempo: %f\n",(score/totscore*10), score,totscore, segundos);
+	printf("\nPresiona enter para continuar.\n");
+	getchar();
 }
 
-
+/*
+*	@Brief/resumen		La función regresa una letra dependiendo del contador que se envia, para asignarla a 
+*				a la respuesta de las preguntas durante el examen. El contador determina la letra.	
+*	@Author/autor		Bruno Valerio Fernández, Abdín Alejandro Alcazar Terán, Benjamín Menchaca Reyna
+*	@Return 		void
+*/
 char CSORT(int cont2)
 {
 	if(cont2==0)
@@ -298,7 +316,14 @@ char CSORT(int cont2)
 		return 'D';
 }
 
-void RVALUE(int numlin, int rvalue[])//Igual que la funcion RVALANS; pero cambia las direcciones de rvalue[].
+/*
+*	@Brief/resumen		La funcion llena un arreglo con valores aleatorios irrepetibles, que dependen del numero 
+				numlin enviado. Esta funcion genera el arreglo aleatorio de preguntas al resolver el examen y 
+				se llama cada vez que se mande llamar una pregunta para revolver sus respuestas.
+*	@Author/autor		Bruno Valerio Fernández, Abdín Alejandro Alcazar Terán, Benjamín Menchaca Reyna
+*	@Return 		void
+*/
+void RVALUE(int numlin, int rvalue[])//genera valores irrepetibles aleatorios dentro de un arreglo.
 {
 	int cont,x,aux=0;
 	int key;
@@ -309,10 +334,10 @@ void RVALUE(int numlin, int rvalue[])//Igual que la funcion RVALANS; pero cambia
 		key=0;		
 		do
 		{
-			x=rand()%numlin;
-			for(aux=0;aux<=cont;aux++)
+			x=rand()%numlin;//Se genera un valor aleatorio.
+			for(aux=0;aux<=cont;aux++)//Se busca dentro del arreglo si el numero aleatorio ya existe.
 			{
-				if(aux==cont)
+				if(aux==cont)//Si se llega al ultimo elemento del arreglo, se 
 				key=1;				
 				if(rvalue[aux]==x)
 					break;
@@ -325,80 +350,14 @@ void RVALUE(int numlin, int rvalue[])//Igual que la funcion RVALANS; pero cambia
 	return;
 }
 
-void RVALANS(int numlin, int rvalans[])//Igual que la funvion RVALUE: pero cambia los valores de RVALANS
-{
-	int cont=0,x=0,aux=0;
-	int key=0;
-	srand((unsigned)time(NULL));
-
-	for(cont=0;cont<numlin;cont++)
-	{
-		key=0;		
-		do
-		{
-			x=rand()%numlin+1;
-			for(aux=0;aux<=cont;aux++)
-			{
-				if(aux==cont)
-				key=1;				
-				if(rvalans[aux]==x)
-					break;
-				
-			}
-		}while(key==0);
-		rvalans[cont]=x;
-		//printf("%d ",rvalans[cont]);	
-	}
-	return;
-}
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
-*	@Brief/resumen		La función permite recibir la información del aplicador de 
-*						exámenes, y evalúa las respuestas correctas del alumno y envía 
-*						la calificación a un archivo en el cual se tendrá la base de 
-*						datos genera.			
+*	@Brief/resumen		Esta funcion regresa el numero de lineas de un archivo. Se utiliza para
+				conocer el numero de preguntas, en el archivo de examen y en el banco.
 *	@Author/autor		Bruno Valerio Fernández, Abdín Alejandro Alcazar Terán, Benjamín Menchaca Reyna
-*	@Return 				void
+*	@Return 		int
 */
 
-/*
-*	@Brief/resumen		La función permite generar un archivo para leer las líneas.
-*	@Author/autor		Bruno Valerio Fernández, Abdín Alejandro Alcazar Terán, Benjamín Menchaca Reyna
-*	@Return 		contador_1: es el contador de líneas por archivo.
-*/
 int archivoLineas(char *nombreArchivo)
 {
 
@@ -412,13 +371,7 @@ int archivoLineas(char *nombreArchivo)
 		contador_1++;	
 	}
 
-	if(contador_1<5)
-	{
-		printf("\n\tEs recomendable tener un minimo de 5 preguntas escritas en el banco de preguntas.\n\tAgregar mas preguntas o utilizar otro archivo para continuar.\n");
-		exit(1);
-	}
 	fclose(Bancopreg);
 	return contador_1;
 }
-
 
